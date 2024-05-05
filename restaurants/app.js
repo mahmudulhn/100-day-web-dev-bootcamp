a32098a5198e6b1967d0791cb495257ba88f1cae
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-05-05 23:42:22 +0600

    19 - More Express Static & Dynamic Content with Templates EJS Days 49 51

---
+++ restaurants/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/restaurants', (req, res) => {
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    res.render('restaurants', {
+        numberOfRestaurants: storedRestaurants.length,
+        restaurants: storedRestaurants,
+    });
+});
+
+app.get('/recommend', (req, res) => {
+    res.render('recommend');
+});
+
+app.post('/recommend', (req, res) => {
+    const restaurant = req.body;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    storedRestaurants.push(restaurant);
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    res.redirect('/confirm');
+})
+
+app.get('/confirm', (req, res) => {
+    res.render('confirm');
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+
+});
+
+
+
+app.listen(3000);
+
