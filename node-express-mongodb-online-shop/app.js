98f77851a96a261e238fa3cf29c5638c57fbc4b7
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-05-24 21:44:24 +0600

    575 - Working On The Shopping Cart Logic Day 89

--- node-express-mongodb-online-shop/app.js
+++ node-express-mongodb-online-shop/app.js
@@ -9,6 +9,9 @@ const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
+
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
@@ -28,12 +31,15 @@ const sessionConfig = createSessionConfig();
 app.use(expressSession(sessionConfig));
 app.use(csrf());
 
+app.use(cartMiddleware);
+
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
 
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use(protectRoutesMiddleware);
 app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
