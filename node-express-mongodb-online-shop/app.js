0b2c090c9014cd22eac8c8e6d31a01670d0eac9f
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-05-25 19:05:19 +0600

    32 - Milestone Project A Complete Online Shop From A to Z Days 78 92

--- node-express-mongodb-online-shop/app.js
+++ node-express-mongodb-online-shop/app.js
@@ -11,11 +11,15 @@ const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
 const cartMiddleware = require('./middlewares/cart');
+const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
+const notFoundHandlerMiddleware = require('./middlewares/not-found');
 
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
 const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
+const ordersRoutes = require('./routes/orders.routes');
 
 const app = express();
 
@@ -25,6 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
 
@@ -32,6 +37,7 @@ app.use(expressSession(sessionConfig));
 app.use(csrf());
 
 app.use(cartMiddleware);
+app.use(updateCartPricesMiddleware);
 
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
@@ -39,8 +45,11 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
-app.use(protectRoutesMiddleware);
-app.use('/admin', adminRoutes);
+app.use('/cart', cartRoutes);
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundHandlerMiddleware);
 
 app.use(errorHandlerMiddleware);
 
--- node-express-mongodb-online-shop/routes/products.routes.js
+++ node-express-mongodb-online-shop/routes/products.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const router = express.Router();
-
 const productsController = require('../controllers/products.controller');
+
+const router = express.Router();
 
 router.get('/products', productsController.getAllProducts);
 
